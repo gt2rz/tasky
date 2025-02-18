96babdf90baa17e69de9cf967d182f8bc0dad755
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-02-17 20:53:38 -0500

    Update landing page layout; enhance structure with flexbox for better alignment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold">Tasky</h1>
       <h6>This is a landing page</h6>
-    </>
+    </div>
   );
 }
