4fae9d9c3eba895faa9e7e83159a6dc016c519fa
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-02-19 07:31:38 -0500

    Refactor layout components; rename and enhance authentication and landing layouts for improved structure

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <section className="w-full h-full flex items-center justify-center">
+        {children}
+      </section>
+      <section className="w-full h-full flex items-center justify-center bg-zinc-900">
+        {/* Here image */}
+      </section>
+    </main>
+  );
 };
 
-export default layout;
+export default AuthLayout;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const LandingLayout = ({ children }: { children: ReactNode }) => {
   return <main className="w-full min-h-screen flex flex-col">{children}</main>;
 };
 
-export default layout;
+export default LandingLayout;
