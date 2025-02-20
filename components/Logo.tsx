2bc24d9e987a378f451bfb6f6e74deeb3cf614ca
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-02-19 20:39:38 -0500

    Add Logo component for branding display

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <h1 className="text-4xl font-bold font-sigmar flex w-full justify-center">
+      tasky<span className="text-orange-400">!</span>
+    </h1>
+  );
+};
+
+export default Logo;
