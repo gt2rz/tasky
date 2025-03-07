4ad529fcc593c0d8199dab1d1a172e1d5a0fe2b8
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-03-07 11:31:28 -0500

    Enhance dashboard layout with header navigation and sidebar; update global styles for improved theming

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,6 @@
-import Logo from "@/components/Logo";
-
 export default function Dashboard() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-      <Logo />
       <h6>This is a dashboard page</h6>
     </div>
   );
--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,14 +1,58 @@
+import Logo from "@/components/Logo";
 import React, { ReactNode } from "react";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  AddCircleIcon,
+  DragDropIcon,
+  Folder01Icon,
+  Notification03Icon,
+  OptionIcon,
+  Search01Icon,
+  Setting06Icon,
+} from "@hugeicons/core-free-icons";
 
 const PrivateLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full min-h-screen flex flex-col">
-      <header className="fixed top-0 h-16 bg-black w-full">Header</header>
+      <header className="fixed top-0 bg-black w-full p-4">
+        <div className="flex flex-1 gap-4 items-center">
+          <Logo />
+          <nav className="flex flex-1 justify-between">
+            <ul className="flex gap-4">
+              <li>Start</li>
+              <li>Projects</li>
+              <li>Planning</li>
+              <li>Calendar</li>
+              <li>Team</li>
+            </ul>
+            <ul className="flex gap-4">
+              <li>
+                <HugeiconsIcon icon={Search01Icon} />
+              </li>
+              <li>
+                <HugeiconsIcon icon={DragDropIcon} />
+              </li>
+              <li>
+                <HugeiconsIcon icon={AddCircleIcon} />
+              </li>
+              <li>
+                <HugeiconsIcon icon={Folder01Icon} />
+              </li>
+              <li>
+                <HugeiconsIcon icon={Notification03Icon} />
+              </li>
+              <li>Logout</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
       <div className="flex flex-1 flex-col sm:flex-row">
-        <main className="flex-1 bg-indigo-100">{children}</main>
-        <nav className="order-first sm:w-32 bg-purple-200">Sidebar</nav>
+        <main className="flex-1 bg-background-900">{children}</main>
+        <nav className="order-first sm:w-32 bg-background-800">Sidebar</nav>
       </div>
-      <footer className="w-full h-8 fixed bottom-0 bg-gray-100">Footer</footer>
+      <footer className="w-full h-8 fixed bottom-0 bg-background-700">
+        Footer
+      </footer>
     </div>
   );
 };
