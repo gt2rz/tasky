8a7e00577f31cd6cba16c968e56376b3d0936056
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-03-07 16:53:45 -0500

    Enhance user profile interaction; update styling for profile image and hover effect

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -22,7 +22,7 @@ const ProtectedLayout = ({ children }: { children: ReactNode }) => {
               className="rounded-full ring ring-black w-8 h-8"
               src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             />
-            <span className="text-gray-400">
+            <span className="text-gray-400 hover:text-white cursor-pointer">
               <HugeiconsIcon icon={ArrowDown01Icon} />
             </span>
           </div>
