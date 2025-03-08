fbd3c348da12204bc16645641f2cba539473e616
Miguel Gutierrez <gt2rz.dev@gmail.com>
2025-03-07 19:14:43 -0500

    Add login link to landing page for user navigation

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import Logo from "@/app/components/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <Logo />
       <h6>This is a landing page</h6>
+      <Link href="/login">Login</Link>
     </div>
   );
 }
